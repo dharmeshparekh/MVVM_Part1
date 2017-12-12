145914e40d4178dfa722eef35bd332675195eb2d
DXamarin <DXamarin@outlook.com>
2017-12-12 12:25:58 +0530

    Add project files.

---
+++ MVVM_Part1/MVVM_Part1/Models/Employeelist.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms;
+
+namespace MVVM_Part1.Models
+{
+    public class Employeelist : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Title { get; set; }
+
+        private bool _isActive;
+        public bool IsActive
+        {
+            get { return _isActive; }
+            set
+            {
+                if (_isActive == value)
+                    return;
+
+                _isActive = value;
+
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Color));
+            }
+        }
+
+        public Color Color
+        {
+            get { return IsActive ? Color.Pink : Color.Black; }
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MVVM_Part1/MVVM_Part1/Views/EmployeeListPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using MVVM_Part1.Models;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MVVM_Part1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EmployeeListPage : ContentPage
+    {
+        private ObservableCollection<Employeelist> _employeeList = new ObservableCollection<Employeelist>();
+        public EmployeeListPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            employeelistsListView.ItemsSource = _employeeList;
+            
+            base.OnAppearing();
+        }
+
+        void OnAddEmployeelist(object sender, System.EventArgs e)
+        {
+            var newEmployeelist = "Employee " + (_employeeList.Count + 1);
+
+            _employeeList.Add(new Employeelist { Title = newEmployeelist });
+
+            this.Title = $"{_employeeList.Count} Employees";
+        }
+
+        void OnEmployeelistSelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+                return;
+
+            var employeelist = e.SelectedItem as Employeelist;
+            employeelist.IsActive = !employeelist.IsActive;
+
+            employeelistsListView.SelectedItem = null;
+                        
+        }
+    }
+}
